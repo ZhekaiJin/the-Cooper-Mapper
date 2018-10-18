079fd38f9f1760b3ccdc781c4c471f72755fb2a6
Zhekai Jin (Scott) <jinzhekai0705@163.com>
2018-10-18 17:08:08 -0400

    Update intuition_in_1D.py

--- localization/src/intuition_in_1D.py
+++ localization/src/intuition_in_1D.py
@@ -1,21 +1,16 @@
 #2018-10-18
-#Scott
-
-
+#Scott Jin
 p=[0.2, 0.2, 0.2, 0.2, 0.2] #Initial Belief where everything is unknown ==> Uniform Distribution
 world=['green', 'red', 'red', 'green', 'green'] # The world as Ground Truth ===> A Map 
 measurements = ['red', 'red']  # Series of Measurment coming in
 motions = [1,1] #Information from Encoder about How far we moved 
-
 #Measurement with uncertainty Encoded 
 pHit = 0.6
 pMiss = 0.2
-
 #Movement parameter with uncertainty 
 pExact = 0.8
 pOvershoot = 0.1 #overshoot by 1 grid 
 pUndershoot = 0.1 #undershoot by 1 grid 
-
 def sense(p, Z):
     q=[]
     for i in range(len(p)):
@@ -26,7 +21,6 @@ def sense(p, Z):
     for i in range(len(q)):
         q[i] = q[i] / s
     return q
-
 def move(p, U):
     q = []
     for i in range(len(p)):
@@ -35,10 +29,8 @@ def move(p, U):
         s = s + pUndershoot * p[(i-U+1) % len(p)] # UnderShooting
         q.append(s)
     return q
-
 #Now with the move and sense we can update our belief and essentially do the localization in  this simplified world.
 for k in range(len(measurements)):
     p = sense(p, measurements[k])
-    p = move(p, motions[k])
-    
+    p = move(p, motions[k]) 
 print p         
