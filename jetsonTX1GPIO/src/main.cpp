2f1531b809a45f00bebf26440c5948fb02311329
shaoyifei96 <shaoyifei96@gmail.com>
2018-10-27 13:41:40 -0400

    added launch file

--- jetsonTX1GPIO/src/main.cpp
+++ jetsonTX1GPIO/src/main.cpp
@@ -8,11 +8,12 @@ int main(int argc, char** argv) {
 	ros::init(argc,argv,"gpio_encoder");
 	if (argc != 4 ) return 1;// 1: l or r 2: pinA 3: pinB
 	ros::NodeHandle n;
+	//TODO: remap string to jetsonTX1GPIONumber
 	jetsonTX1GPIONumber A = pin13;
 	jetsonTX1GPIONumber B = pin33;
 	Encoder enc_handle(A,B);
 	enc_handle.pub = n.advertise<std_msgs::Int16>(strcat(argv[1] ,"wheel"), 1);
-	ros::Rate loop_rate(10);
+	ros::Rate loop_rate(300);
 	while (ros::ok()) {
 		enc_handle.monitorEncoder();
 		std_msgs::Int16 msg;
